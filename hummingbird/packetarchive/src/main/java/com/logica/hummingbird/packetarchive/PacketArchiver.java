95bfc7b826a66152ef12ab3f707d9493df3d7477
Mark Doyle <markjohndoyle@googlemail.com>
2010-03-22 09:56:35 +0100

    Added application start point.

---
+++ hummingbird/packetarchive/src/main/java/com/logica/hummingbird/packetarchive/PacketArchiver.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.logica.hummingbird.packetarchive;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author doylemr
+ *
+ */
+public class PacketArchiver {
+
+	public static void main(String[] args) {
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("spring-config/packet-archiver-spring-context.xml");
+	}
+}
