1a036602ffcbaa591322934f3a4b106621110863
Mark Doyle <markjohndoyle@googlemail.com>
2010-03-25 10:54:57 +0100

    Shouldn't catch Exceptions in a Junit test unless checking for exception handling.

--- hummingbird/xtce/src/test/java/com/logica/hummingbird/xtce/XtceTest.java
+++ hummingbird/xtce/src/test/java/com/logica/hummingbird/xtce/XtceTest.java
@@ -1,35 +1,29 @@
 package com.logica.hummingbird.xtce;
 
-
 import org.junit.Before;
 import org.junit.Test;
 
 public class XtceTest {
 
 	XtceModelFactory xtceFactory;
-	
+
 	@Before
 	public void setUp() throws Exception {
-    	xtceFactory = new XtceModelFactory();
-    	xtceFactory.setSpacesystemmodelFilename("src/main/resources/humsat.xml");
+		xtceFactory = new XtceModelFactory();
+		xtceFactory.setSpacesystemmodelFilename("src/main/resources/humsat.xml");
 	}
-	
+
 	@Test
-	public void TestContainers() {
-		
+	public void TestContainers() throws Exception {
+
 		xtceFactory.initialise();
 		System.out.println(xtceFactory.getAllParameters());
-		
-		try {
-			System.out.println("TMFrameHeader: " +xtceFactory.getContainer("TMFrameHeader").getSubContainers());
-			System.out.println("TMFrameTail: " +xtceFactory.getContainer("TMFrameTail").getSubContainers());
-			
-			System.out.println("Parameters: " +xtceFactory.getAllParameters().values());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		
+
+		System.out.println("TMFrameHeader: " + xtceFactory.getContainer("TMFrameHeader").getSubContainers());
+		System.out.println("TMFrameTail: " + xtceFactory.getContainer("TMFrameTail").getSubContainers());
+
+		System.out.println("Parameters: " + xtceFactory.getAllParameters().values());
+
 	}
 
 }
