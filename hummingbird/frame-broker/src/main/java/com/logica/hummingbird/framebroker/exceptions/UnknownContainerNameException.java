36ccd635f12a41092c571208cafbe8635021a39a
Mark Doyle <markjohndoyle@googlemail.com>
2010-03-25 11:41:21 +0100

    New Exception for the FrameBroker Containers.
    I forgot to check this in!

---
+++ hummingbird/frame-broker/src/main/java/com/logica/hummingbird/framebroker/exceptions/UnknownContainerNameException.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package com.logica.hummingbird.framebroker.exceptions;
+
+import java.util.Map;
+import java.util.Set;
+
+import com.logica.hummingbird.framebroker.Container;
+
+/**
+ * This is a checked exception class which will allow the client to take steps to 
+ * rectify the situation.  There is a useful method included which will allow the
+ * client to retrieve the available Container names.
+ * 
+ * @author Mark Doyle
+ *
+ */
+public class UnknownContainerNameException extends Exception {
+	private static final long serialVersionUID = 3499163979287233832L;
+	
+	Map<String, Container> containers = null;
+
+	/**
+	 * 
+	 */
+	public UnknownContainerNameException(Map<String, Container> containers) {
+		this.containers = containers;
+	}
+
+	/**
+	 * @param message
+	 */
+	public UnknownContainerNameException(Map<String, Container> containers, String message) {
+		super(message);
+		this.containers = containers;
+	}
+
+	/**
+	 * @param cause
+	 */
+	public UnknownContainerNameException(Map<String, Container> containers, Throwable cause) {
+		super(cause);
+		this.containers = containers;
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public UnknownContainerNameException(Map<String, Container> containers, String message, Throwable cause) {
+		super(message, cause);
+		this.containers = containers;
+	}
+
+	public Set<String> getAvailableContainerNames() {
+		return containers.keySet();
+	}
+}
