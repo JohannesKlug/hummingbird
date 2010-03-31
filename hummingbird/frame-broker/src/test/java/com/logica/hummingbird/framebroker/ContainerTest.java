a3e358feec2fe89248532ebd42f418db18d638c0
Mark Doyle <markjohndoyle@googlemail.com>
2010-03-31 17:27:38 +0200

    Improved tests.
    Nearly complete now!

--- hummingbird/frame-broker/src/test/java/com/logica/hummingbird/framebroker/ContainerTest.java
+++ hummingbird/frame-broker/src/test/java/com/logica/hummingbird/framebroker/ContainerTest.java
@@ -5,10 +5,16 @@ package com.logica.hummingbird.framebroker;
 
 import static org.junit.Assert.fail;
 
+import java.util.BitSet;
+
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.logica.hummingbird.framebroker.exceptions.UnknownContainerNameException;
+import com.logica.hummingbird.framebroker.parameters.Parameter;
 
 /**
  * @author doylemr
@@ -20,6 +26,8 @@ public class ContainerTest {
 	 */
 	private static final Logger LOG = LoggerFactory.getLogger(ContainerTest.class);
 
+	private static final MockContainerModelFactory MOCK_CONTAINER_MODEL_FACTORY = new MockContainerModelFactory();
+
 	private static IFrameBroker testFrameBroker = null;
 
 	/**
@@ -30,27 +38,51 @@ public class ContainerTest {
 		if (LOG.isDebugEnabled()) {
 			LOG.debug("Setting up for all tests");
 		}
-		testFrameBroker = new ContainerProcessor(new MockContainerModelFactory());
+		testFrameBroker = new ContainerProcessor(MOCK_CONTAINER_MODEL_FACTORY);
 	}
 
 	/**
 	 * Test method for
 	 * {@link com.logica.hummingbird.framebroker.Container#unmarshall(java.util.BitSet)}
 	 * .
+	 * 
+	 * @throws UnknownContainerNameException
 	 */
-	@Test
-	public void testUnmarshall() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void testUnmarshall() throws UnknownContainerNameException {
+//		 testFrameBroker.unmarshall("TMFrame", frame);
+//	}
 
 	/**
 	 * Test method for
 	 * {@link com.logica.hummingbird.framebroker.Container#marshall(java.util.BitSet, int)}
 	 * .
+	 * 
+	 * @throws UnknownContainerNameException
 	 */
 	@Test
-	public void testMarshall() {
-		fail("Not yet implemented");
+	public void testMarshall() throws UnknownContainerNameException {
+		LOG.debug(MOCK_CONTAINER_MODEL_FACTORY.getContainer("TMFrame").toString());
+
+		/** Populate the input mock frame **/
+		// Set the Apid to Packet ID type A
+		Parameter apid = MOCK_CONTAINER_MODEL_FACTORY.getParameter(MockContainerModelFactory.PACKET_ID_NAME);
+		apid.setValue(Float.valueOf(MockContainerModelFactory.PACKET_TYPE_A_ID));
+
+		// Set parameter A value
+		Parameter paramA = MOCK_CONTAINER_MODEL_FACTORY.getParameter(MockContainerModelFactory.TEST_PARAM_VALUE_A);
+		paramA.setValue(123);
+
+		int length = MOCK_CONTAINER_MODEL_FACTORY.getContainer("TMFrame").getLength();
+		if (LOG.isDebugEnabled()) {
+			LOG.debug("TMFrame length = " + length);
+		}
+		
+		BitSet frame = new BitSet(length);
+
+		testFrameBroker.marshall("TMFrame", frame);
+		
+		LOG.debug(BitSetUtility.binDump(frame));
 	}
 
 }
--- hummingbird/frame-broker/src/test/java/com/logica/hummingbird/framebroker/MockContainerModelFactory.java
+++ hummingbird/frame-broker/src/test/java/com/logica/hummingbird/framebroker/MockContainerModelFactory.java
@@ -16,16 +16,28 @@ import com.logica.hummingbird.framebroker.parameters.ParameterType;
 import com.logica.hummingbird.framebroker.parameters.ParameterType.eParameterType;
 
 /**
+ * This class is used for testing the Frame broker.  It creates a simple Container model which can be 
+ * populated with values by tests.  The container model follows the Frame Broker rules and 
+ * therefore contains a TMFrame, TMFrameHeader, TMPacket, and a TMFrameTail.
  * 
- * @author Mark Doyle
- * 
+ * @author Mark Doyle <markjohndoyle@googlemail.com>
+ * @since Hummingbird 0.0.1
  */
 public class MockContainerModelFactory implements IContainerFactory {
-
 	/**
 	 * Logger for this class
 	 */
 	private static final Logger LOG = LoggerFactory.getLogger(MockContainerModelFactory.class);
+
+	public static final String TEST_PARAM_VALUE_A = "Test param value A";
+	
+	public static final String TEST_PARAM_VALUE_B = "Test param value B";
+
+	public static final String PACKET_ID_NAME = "ApId";
+
+	public static final String PACKET_TYPE_B_ID = "333";
+
+	public static final String PACKET_TYPE_A_ID = "555";
 
 	private Map<String, Container> containers = new HashMap<String, Container>();
 	private Map<String, Parameter> parameters = new HashMap<String, Parameter>();
@@ -66,12 +78,10 @@ public class MockContainerModelFactory implements IContainerFactory {
 		Container tmPacketHeader = new Container("TMPacketHeader", "Test packet header", "Test TM packet header for unit testing");
 		this.addToContainers(tmPacketHeader);
 
-		// Create the apid (ID) parameter type and add it to the packet header
-		// and containers collection.
-		ParameterType paramType11bitInt = new ParameterType("11bitInt", "11bit integer type", "Parameter type for 11bit integers", eParameterType.INTEGER,
-				false, 0, 11);
-		IntegerParameter packetTypeId = new IntegerParameter("ApId", "Test Apid", "Test Application Id", paramType11bitInt, 0);
-		this.addToContainers(packetTypeId);
+		// Create the apid (ID) parameter type and add it to the packet header and the parameters collection.
+		ParameterType paramType11bitInt = new ParameterType("11bitInt", "11bit integer type", "Parameter type for 11bit integers", eParameterType.INTEGER, false, 0, 11);
+		IntegerParameter packetTypeId = new IntegerParameter(PACKET_ID_NAME, "Test Apid", "Test Application Id", paramType11bitInt, 0);
+		this.addToParameters(packetTypeId);
 		tmPacketHeader.addContainer(packetTypeId);
 
 		// Create the packet body and add it to the containers collection
@@ -81,30 +91,30 @@ public class MockContainerModelFactory implements IContainerFactory {
 		// Create a packet type, add it to the packet body, add a restriction to
 		// associate it to an ID, and add it to the containers collection
 		Container packetTypeA = new Container("TMPacketTypeA", "Test packet body", "Test TM packet for unit testing");
-		packetTypeA.addRestriction(packetTypeId, "555");
+		packetTypeA.addRestriction(packetTypeId, PACKET_TYPE_A_ID);
 		this.addToContainers(packetTypeA);
 		tmPacketBody.addContainer(packetTypeA);
 
 		// Create a parameter for packetTypeA and add it to the packet type and
 		// the container collection
-		ParameterType test32bitInt = new ParameterType("test32bitInt", "test param", "32 bit int test param", eParameterType.INTEGER, true, 0, 32);
-		IntegerParameter testParameterA = new IntegerParameter("Test param value", "test param", "test param holding an int value", test32bitInt, 5000);
+		ParameterType test32bitInt = new ParameterType("test32bitInt", "test param", "32 bit int test param", eParameterType.INTEGER, false, 0, 32);
+		IntegerParameter testParameterA = new IntegerParameter(TEST_PARAM_VALUE_A, "test param", "test param holding an int value", test32bitInt, 0);
 		packetTypeA.addContainer(testParameterA);
-		this.addToContainers(testParameterA);
+		this.addToParameters(testParameterA);
 
 		// Create a packet type, add it to the packet body, add a restriction to
 		// associate it to an ID, and add it to the containers collection
 		Container packetTypeB = new Container("TMPacketTypeB", "Test packet body", "Test TM packet for unit testing");
-		packetTypeB.addRestriction(packetTypeId, "333");
+		packetTypeB.addRestriction(packetTypeId, PACKET_TYPE_B_ID);
 		this.addToContainers(packetTypeB);
 		tmPacketBody.addContainer(packetTypeB);
 
 		// Create a parameter for packetTypeB and add it to the packet type and
 		// the container collection
-		ParameterType test64bitFloat = new ParameterType("test64bitFloat", "test param", "64 float test param", eParameterType.FLOAT, true, 0, 64);
-		FloatParameter testParameterB = new FloatParameter("Test param value", "test param", "test param holding a float value", test64bitFloat, 43.53f);
+		ParameterType test64bitFloat = new ParameterType("test64bitFloat", "test param", "64 float test param", eParameterType.FLOAT, false, 0, 64);
+		FloatParameter testParameterB = new FloatParameter(TEST_PARAM_VALUE_B, "test param", "test param holding a float value", test64bitFloat, 0);
 		packetTypeA.addContainer(testParameterB);
-		this.addToContainers(testParameterB);
+		this.addToParameters(testParameterB);
 
 		// Add the packet inners (header and body) to the packet
 		List<IContainer> packetInners = new ArrayList<IContainer>(2);
@@ -156,6 +166,11 @@ public class MockContainerModelFactory implements IContainerFactory {
 	 */
 	public void addToContainers(Container container) {
 		this.containers.put(container.getName(), container);
+	}
+	
+
+	private void addToParameters(Parameter parameter) {
+		this.parameters.put(parameter.name, parameter);
 	}
 
 }
