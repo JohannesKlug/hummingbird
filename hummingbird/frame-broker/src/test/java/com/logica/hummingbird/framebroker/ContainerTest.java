945d2baf88de6dfdfd01d8548f5100d76ed51308
Mark Doyle <markjohndoyle@googlemail.com>
2010-04-03 01:19:39 +0200

    Improved the test further by adding a new test method for unmarshalling.

--- hummingbird/frame-broker/src/test/java/com/logica/hummingbird/framebroker/ContainerTest.java
+++ hummingbird/frame-broker/src/test/java/com/logica/hummingbird/framebroker/ContainerTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.logica.hummingbird.framebroker.exceptions.BitSetOperationException;
 import com.logica.hummingbird.framebroker.exceptions.UnknownContainerNameException;
 import com.logica.hummingbird.framebroker.parameters.Parameter;
 
@@ -34,6 +35,8 @@ public class ContainerTest {
 			+ System.getProperty("line.separator")
 			+ "00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 "
 			+ System.getProperty("line.separator");
+	
+	private final static String BITSET_AS_STRING = "11010100010110111100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
 
 	/**
 	 * Set's up the FrameBroker with a mock container model factory.
@@ -52,11 +55,15 @@ public class ContainerTest {
 	 * .
 	 * 
 	 * @throws UnknownContainerNameException
+	 * @throws BitSetOperationException 
 	 */
-	// @Test
-	// public void testUnmarshall() throws UnknownContainerNameException {
-	// testFrameBroker.unmarshall("TMFrame", frame);
-	// }
+	@Test
+	public void testUnmarshall() throws UnknownContainerNameException, BitSetOperationException {
+		LOG.info("Beginning test");
+		
+		BitSet frame = BitSetUtility.fromString(BITSET_AS_STRING);
+		testFrameBroker.unmarshall("TMFrame", frame);
+	}
 
 	/**
 	 * Test method for
@@ -67,6 +74,8 @@ public class ContainerTest {
 	 */
 	@Test
 	public void testMarshall() throws UnknownContainerNameException {
+		LOG.info("Beginning test");
+		
 		LOG.debug(MOCK_CONTAINER_MODEL_FACTORY.getContainer("TMFrame").toString());
 
 		/** Populate the input mock frame **/
@@ -78,7 +87,8 @@ public class ContainerTest {
 		Parameter paramA = MOCK_CONTAINER_MODEL_FACTORY.getParameter(MockContainerModelFactory.TEST_PARAM_VALUE_A);
 		paramA.setValue(123);
 
-		// Get the frame length, that is, the sum of itself and it's tree of sub containers and set the bitset to this length.
+		// Get the frame length, that is, the sum of itself and it's tree of sub
+		// containers and set the bitset to this length.
 		int length = MOCK_CONTAINER_MODEL_FACTORY.getContainer("TMFrame").getLength();
 		if (LOG.isDebugEnabled()) {
 			LOG.debug("TMFrame length = " + length);
@@ -92,26 +102,29 @@ public class ContainerTest {
 		String binDump = BitSetUtility.binDump(frame);
 		LOG.debug(binDump);
 
-		// Assert the bitset is as expected.  // TODO there is probably a better way to do this than a binDump String comparison haha. 
+		// Assert the bitset is as expected. // TODO there is probably a better
+		// way to do this than a binDump String comparison haha.
 		assertEquals("Bit dump not equal", EXPECTED_BIT_SET_DUMP, binDump);
-		
 
 		// Now let's marshall a parameter and run some tests.
 		BitSet apidBitSet = new BitSet(MOCK_CONTAINER_MODEL_FACTORY.getParameter(MockContainerModelFactory.PACKET_ID_NAME).getLength());
 		testFrameBroker.marshall(MockContainerModelFactory.PACKET_ID_NAME, apidBitSet);
 		String apidBinDump = BitSetUtility.binDump(apidBitSet);
-		if(LOG.isDebugEnabled()) {
-			LOG.debug(MOCK_CONTAINER_MODEL_FACTORY.getParameter(MockContainerModelFactory.PACKET_ID_NAME) + " = " + apidBinDump);
+		if (LOG.isDebugEnabled()) {
+			LOG.debug(MOCK_CONTAINER_MODEL_FACTORY.getParameter(MockContainerModelFactory.PACKET_ID_NAME) + " = "
+					+ apidBinDump);
 		}
-		
+
 		String apidBinValue = Integer.toBinaryString(Integer.valueOf(MockContainerModelFactory.PACKET_TYPE_A_ID));
-		 // LSB first so we need to reverse the dumped string.
+		// LSB first so we need to reverse the dumped string.
 		apidBinValue = new StringBuffer(apidBinValue).reverse().toString();
-		// BitSets are rounded up to 64 bits so we will have to pad the marshalled APID with extra 0's
-		// We will calculate the required zeros using the apid's length so we don't spoil the test
+		// BitSets are rounded up to 64 bits so we will have to pad the
+		// marshalled APID with extra 0's
+		// We will calculate the required zeros using the apid's length so we
+		// don't spoil the test
 		int requiredPadding = 64 - apidBinValue.length();
 		char[] zeroPadding = new char[requiredPadding];
-		for(int c = 0; c < requiredPadding; c++) {
+		for (int c = 0; c < requiredPadding; c++) {
 			zeroPadding[c] = '0';
 		}
 		StringBuffer apidBinValueWithPadding = new StringBuffer(apidBinValue);
