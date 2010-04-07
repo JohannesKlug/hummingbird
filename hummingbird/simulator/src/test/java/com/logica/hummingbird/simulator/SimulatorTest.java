4055835524bca8b6c1c5958e9f6f43749b1ce5aa
Johannes Klug <johannes.klug@logica.com>
2010-04-07 14:31:18 +0200

    FlatSimulator added, tested

--- hummingbird/simulator/src/test/java/com/logica/hummingbird/simulator/SimulatorTest.java
+++ hummingbird/simulator/src/test/java/com/logica/hummingbird/simulator/SimulatorTest.java
@@ -1,6 +1,7 @@
 package com.logica.hummingbird.simulator;
 
 import org.apache.camel.EndpointInject;
+import org.apache.camel.Exchange;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
@@ -12,6 +13,8 @@ public class SimulatorTest extends CamelTestSupport {
     
     @Test
     public void TestSimulator() throws Exception {
+    	
+    	resultEndpoint.reset();
     	
     	Simulator simulator = new Simulator(resultEndpoint);
     	
@@ -34,6 +37,39 @@ public class SimulatorTest extends CamelTestSupport {
     	
     	resultEndpoint.setExpectedMessageCount(5);
     	resultEndpoint.assertIsSatisfied();
+    }
+    
+    @Test
+    public void TestFlatSim() throws Exception {
+    	
+    	resultEndpoint.reset();
+    	
+    	Simulator simulator = new FlatSimulator(resultEndpoint, 3.141);
+    	
+    	resultEndpoint.setExpectedMessageCount(0);
+    	resultEndpoint.assertIsSatisfied();
+    	
+    	simulator.sendMessage();
+
+    	resultEndpoint.setExpectedMessageCount(1);
+    	resultEndpoint.assertIsSatisfied();
+    	
+    	simulator.sendMessage();
+    	
+    	resultEndpoint.setExpectedMessageCount(2);
+    	resultEndpoint.assertIsSatisfied();
+    	
+    	simulator.sendMessage();
+    	simulator.sendMessage();
+    	simulator.sendMessage();
+    	
+    	resultEndpoint.setExpectedMessageCount(5);
+    	resultEndpoint.assertIsSatisfied();
+    	
+    	for (Exchange exchange : resultEndpoint.getExchanges()) {
+    		assertEquals(3.141, exchange.getOut().getBody());
+    	}
+    	
     }
 
 }
