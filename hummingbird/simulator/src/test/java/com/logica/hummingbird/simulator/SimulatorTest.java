6d07b352ea6d66ed7916f47c52ec9ffbd1a13b70
Johannes Klug <jk@jklug.com>
2010-04-08 17:26:04 +0200

    Removed FlatSim - refactoring to use Waveforms

--- hummingbird/simulator/src/test/java/com/logica/hummingbird/simulator/SimulatorTest.java
+++ hummingbird/simulator/src/test/java/com/logica/hummingbird/simulator/SimulatorTest.java
@@ -1,7 +1,6 @@
 package com.logica.hummingbird.simulator;
 
 import org.apache.camel.EndpointInject;
-import org.apache.camel.Exchange;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
@@ -37,39 +36,6 @@ public class SimulatorTest extends CamelTestSupport {
     	
     	resultEndpoint.setExpectedMessageCount(5);
     	resultEndpoint.assertIsSatisfied();
-    }
-    
-    @Test
-    public void TestFlatSim() throws Exception {
-    	
-    	resultEndpoint.reset();
-    	
-    	Simulator simulator = new FlatSimulator(resultEndpoint, 3.141);
-    	
-    	resultEndpoint.setExpectedMessageCount(0);
-    	resultEndpoint.assertIsSatisfied();
-    	
-    	simulator.sendMessage();
-
-    	resultEndpoint.setExpectedMessageCount(1);
-    	resultEndpoint.assertIsSatisfied();
-    	
-    	simulator.sendMessage();
-    	
-    	resultEndpoint.setExpectedMessageCount(2);
-    	resultEndpoint.assertIsSatisfied();
-    	
-    	simulator.sendMessage();
-    	simulator.sendMessage();
-    	simulator.sendMessage();
-    	
-    	resultEndpoint.setExpectedMessageCount(5);
-    	resultEndpoint.assertIsSatisfied();
-    	
-    	for (Exchange exchange : resultEndpoint.getExchanges()) {
-    		assertEquals(3.141, exchange.getOut().getBody());
-    	}
-    	
     }
     
     @Test
