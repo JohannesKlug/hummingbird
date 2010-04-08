69304af44582ba7b59e85f801645f1d5fdb098f9
Johannes Klug <jk@jklug.com>
2010-04-08 17:04:04 +0200

    Added Waveform implementation

---
+++ hummingbird/simulator/src/main/java/com/logica/hummingbird/simulator/waveforms/FlatWaveform.java
@@ -0,0 +1,20 @@
+package com.logica.hummingbird.simulator.waveforms;
+
+public class FlatWaveform extends WaveformImpl {
+	
+	
+	private double value;
+	
+	public FlatWaveform(int readings, double value) {
+		super(readings);
+		this.value = value;
+	}
+
+	public double nextValue() {
+		return value;
+	}
+
+
+	
+
+}
---
+++ hummingbird/simulator/src/main/java/com/logica/hummingbird/simulator/waveforms/Waveform.java
@@ -0,0 +1,9 @@
+package com.logica.hummingbird.simulator.waveforms;
+
+public interface Waveform {
+	
+	public int getReadings();
+	
+	public double nextValue();
+
+}
