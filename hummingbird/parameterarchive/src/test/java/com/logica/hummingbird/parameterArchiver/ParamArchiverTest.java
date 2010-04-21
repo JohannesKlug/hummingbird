ed10fa4eb99630b7c0e0046908d57cdaf4afb577
Johannes Klug <johannes.klug@logica.com>
2010-04-21 15:15:43 +0200

    Test for ParameterArchiver started

--- hummingbird/parameterarchive/src/test/java/com/logica/hummingbird/parameterArchiver/ParamArchiverTest.java
+++ hummingbird/parameterarchive/src/test/java/com/logica/hummingbird/parameterArchiver/ParamArchiverTest.java
@@ -2,12 +2,17 @@ package com.logica.hummingbird.parameterArchiver;
 
 import static org.junit.Assert.*;
 
+import com.logica.hummingbird.framebroker.IContainerFactory;
+import com.logica.hummingbird.framebroker.MockContainerModelFactory;
+
 import org.junit.Test;
 
 public class ParamArchiverTest {
 	
 	@Test
 	public void testParameterArchiverConstructor() {
+		IContainerFactory mockCF = new MockContainerModelFactory();
+		System.out.println(mockCF.getAllParameters());
 	}
 	
 }
