a4d5c0a4e7d702703a3c453078e5b3ee4d610e71
Mark Doyle <markjohndoyle@googlemail.com>
2010-06-02 00:45:49 +0200

    Added test skeleton for FrameBrokerImpl.

---
+++ hummingbird/frame-broker/src/test/java/com/logica/hummingbird/framebroker/FrameBrokerImplTest.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package com.logica.hummingbird.framebroker;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Mark Doyle
+ * 
+ */
+public class FrameBrokerImplTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.logica.hummingbird.framebroker.FrameBrokerImpl#unmarshall(java.lang.String, java.util.BitSet)}.
+	 */
+	@Test
+	public final void testUnmarshall() {
+		 // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.logica.hummingbird.framebroker.FrameBrokerImpl#marshall(java.lang.String, java.util.BitSet)}.
+	 */
+	@Test
+	public final void testMarshallStringBitSet() {
+		// TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.logica.hummingbird.framebroker.FrameBrokerImpl#marshall(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public final void testMarshallStringString() {
+		 // TODO
+	}
+
+}
