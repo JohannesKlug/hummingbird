e4c83b14b6fa8a7127b278b613f6ec36d361bf26
Mark Doyle <markjohndoyle@googlemail.com>
2010-06-14 22:42:36 +0200

    Implemented valueFromBitSet in Float64Behaviour.

--- hummingbird/commons/src/main/java/com/logica/hummingbird/spacesystemmodel/parameters/behaviours/Float64Behaviour.java
+++ hummingbird/commons/src/main/java/com/logica/hummingbird/spacesystemmodel/parameters/behaviours/Float64Behaviour.java
@@ -2,12 +2,21 @@ package com.logica.hummingbird.spacesystemmodel.parameters.behaviours;
 
 import java.util.BitSet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.logica.hummingbird.util.BitSetUtility;
+import com.logica.hummingbird.util.BitSetUtility.FloatSizeInBits;
+
 /**
  * Parameter behaviour for an IEEE 754 64-bit precision Float.
+ * 
  * @author Mark Doyle
- *
+ * @author Johannes Klug
+ * 
  */
 public class Float64Behaviour extends AbstractFloatBehaviour {
+	private final static Logger LOG = LoggerFactory.getLogger(Float64Behaviour.class);
 
 	public Float64Behaviour() {
 		super(64l);
@@ -15,8 +24,35 @@ public class Float64Behaviour extends AbstractFloatBehaviour {
 
 	@Override
 	public Double valueFromBitSet(BitSet packet) {
-		// TODO Auto-generated method stub
-		return null;
+		// The technique this method uses is to convert the bitset to a string and then parse it as a double
+		// Awesome tekkers.
+		
+		int offset = 0;
+
+		BitSet actualBitSet = packet.get(offset, offset + (int) getSizeIntBits());
+
+		if (LOG.isDebugEnabled()) {
+			LOG.debug("Float Parameter BitSet taken from bitset in = " + BitSetUtility.binDump(actualBitSet));
+		}
+
+		int length = actualBitSet.size();
+		if (length != FloatSizeInBits.THIRTY_TWO.getSize() && length != FloatSizeInBits.SIXTY_FOUR.getSize()) {
+			LOG.error("A float BitSet of invalid length was passed. This is an error! Size is: " + length);
+		}
+
+		String actualBitSetString = BitSetUtility.bitSetToBinaryString(actualBitSet, false);
+
+		if (LOG.isDebugEnabled()) {
+			LOG.debug("BinaryString representation of actual bitset = " + actualBitSetString);
+		}
+
+		if (LOG.isDebugEnabled()) {
+			LOG.debug("Extracting value using longBitsToDouble");
+		}
+		
+		// Float 64 so convert to a Double
+		return Double.longBitsToDouble(Long.parseLong(actualBitSetString, 2));
+
 	}
 
 	@Override
@@ -29,13 +65,12 @@ public class Float64Behaviour extends AbstractFloatBehaviour {
 	public BitSet insertIntoBitSet(Number number, BitSet bitSetTarget, int offset) {
 		return bitSetTarget;
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public String getTypeName() {
 		return "Double (IEEE754 Double precision 64-bit)";
 	}
-
 
 }
