ccd8b02b77853870c1ee701ab55e3233c246beb8
Mark Doyle <markjohndoyle@googlemail.com>
2010-06-19 22:17:41 +0200

    Implemented Float32Behaviour insertIntoBitSet method.

--- hummingbird/commons/src/main/java/com/logica/hummingbird/spacesystemmodel/parameters/behaviours/Float32Behaviour.java
+++ hummingbird/commons/src/main/java/com/logica/hummingbird/spacesystemmodel/parameters/behaviours/Float32Behaviour.java
@@ -5,6 +5,7 @@ import java.util.BitSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.logica.hummingbird.spacesystemmodel.exceptions.BitSetOperationException;
 import com.logica.hummingbird.util.BitSetUtility;
 
 /**
@@ -49,8 +50,32 @@ public class Float32Behaviour extends AbstractFloatBehaviour {
 	}
 
 	@Override
-	public BitSet insertIntoBitSet(Number number, BitSet bitSetTarget, int offset) {
-		// TODO implement this method
+	public BitSet insertIntoBitSet(Number number, BitSet bitSetTarget, int offset) throws BitSetOperationException {
+		float value = number.floatValue();
+
+		// Convert the value to a bitset
+		// Parse as IEEE-754 Single Precision (32-bit) (Java Integer)
+		int intBits = Float.floatToIntBits(value);
+
+		String binaryString = Integer.toBinaryString(intBits);
+
+		if (value >= 0) {
+			// We have to add the Sign bit manually for positive Numbers
+			binaryString = '0' + binaryString;
+		}
+
+		// Get the BitSet from the String.
+		BitSet valueBitSet = this.bitSetFromString(binaryString);
+
+		// Insert the value BitSet into the target BitSet and return
+		for (int i = 0; i < getSizeIntBits(); i++) {
+			if (valueBitSet.get(i)) {
+				bitSetTarget.set(i + offset);
+			}
+			else {
+				bitSetTarget.clear(i + offset);
+			}
+		}
 		return bitSetTarget;
 	}
 
