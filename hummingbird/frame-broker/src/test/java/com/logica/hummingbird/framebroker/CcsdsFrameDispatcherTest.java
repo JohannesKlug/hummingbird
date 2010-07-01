b6f100771a810d6bef877b521a590b15a647b32a
Johannes Klug <jk@jklug.com>
2010-07-02 01:18:30 +0200

    Organised Imports

--- hummingbird/frame-broker/src/test/java/com/logica/hummingbird/framebroker/CcsdsFrameDispatcherTest.java
+++ hummingbird/frame-broker/src/test/java/com/logica/hummingbird/framebroker/CcsdsFrameDispatcherTest.java
@@ -1,8 +1,8 @@
 package com.logica.hummingbird.framebroker;
 
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;
