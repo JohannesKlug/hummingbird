a8cb294a3c4cc8e5eccabbf2311408615f78a2f0
Johannes Klug <jk@jklug.com>
2010-07-02 01:18:42 +0200

    Using ArrayUtils to clone byte[] passed to constructor

--- hummingbird/frame-broker/src/main/java/com/logica/hummingbird/framebroker/FramePayload.java
+++ hummingbird/frame-broker/src/main/java/com/logica/hummingbird/framebroker/FramePayload.java
@@ -1,12 +1,14 @@
 package com.logica.hummingbird.framebroker;
 
+import org.apache.commons.lang.ArrayUtils;
+
 public class FramePayload {
 	public int vcId;
 	public byte[] payload;
 	
 	public FramePayload(int vcId, byte[] payload) {
 		this.vcId = vcId;
-		this.payload = payload;
+		this.payload = ArrayUtils.clone(payload);
 	}
 
 }
--- hummingbird/frame-broker/src/main/java/com/logica/hummingbird/framebroker/PacketPayload.java
+++ hummingbird/frame-broker/src/main/java/com/logica/hummingbird/framebroker/PacketPayload.java
@@ -1,12 +1,14 @@
 package com.logica.hummingbird.framebroker;
 
+import org.apache.commons.lang.ArrayUtils;
+
 public class PacketPayload {
 	public int apid;
 	public byte[] payload;
 	
 	public PacketPayload(int apid, byte[] payload) {
 		this.apid = apid;
-		this.payload = payload;
+		this.payload = ArrayUtils.clone(payload);
 	}
 
 }
