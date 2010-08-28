7346b5978f17a244e6dc1907cbc0015413171265
Mark Doyle <markjohndoyle@googlemail.com>
2010-08-28 23:47:50 +0200

    Created a little driver class to start a test simulator from the commandline.
    Added some camel dependencies to simulator for driver app.
    Added osgi bundle plugin to tele.

---
+++ hummingbird/simulator/src/main/java/com/logica/hummingbird/simulator/driver/SimpleMessagePump.java
@@ -0,0 +1,34 @@
+package com.logica.hummingbird.simulator.driver;
+
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+
+import com.logica.hummingbird.simulator.Simulator;
+import com.logica.hummingbird.simulator.waveforms.LinearWaveform;
+
+public class SimpleMessagePump {
+	
+	static CamelContext cc = new DefaultCamelContext();
+	
+	public static void main(String[] args) {
+		String uri = args[0];
+		int messageRate = Integer.parseInt(args[1]);
+	
+		
+		try {
+			cc.start();
+		} catch (Exception e1) {
+			System.err.println(e1);
+		}
+		
+		cc.addComponent("activemq", ActiveMQComponent.activeMQComponent(uri));
+		
+		Simulator sim = new Simulator(cc.getEndpoint("activemq:q1"));
+		sim.sendMessage(messageRate);
+		sim.addWaveform(new LinearWaveform(101, 0, 1));
+		
+		Thread simulatorThread = new Thread(sim);
+		simulatorThread.start();
+	}
+}
