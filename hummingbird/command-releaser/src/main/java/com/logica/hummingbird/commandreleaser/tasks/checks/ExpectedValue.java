66a325fe92d8d391935aa4f88a7dfb5870a9a1e5
Gert Villemos <gert.villemos@logica.com>
2010-10-22 14:33:32 +0200

    Commanding init commit

---
+++ hummingbird/command-releaser/src/main/java/com/logica/hummingbird/commandreleaser/tasks/checks/ExpectedValue.java
@@ -0,0 +1,23 @@
+package com.logica.hummingbird.commandreleaser.tasks.checks;
+
+import org.apache.camel.Exchange;
+
+/**
+ * This validation step validates that a specific parameter has a specific value
+ * at the expected time.
+ *
+ */
+public class ExpectedValue extends AbstractCheck {
+
+	protected String parameter = "";
+	protected Object expectedValue = null;
+	
+	public void process(Exchange arg0) {
+		/** Get the value. */
+		
+		/** Check against the limit. */
+		
+		
+		/** Send state parameter. */
+	}
+}
