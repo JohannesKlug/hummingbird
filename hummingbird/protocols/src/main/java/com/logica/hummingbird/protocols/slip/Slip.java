39e5670c425955dbf63951f0da8e075502c78b65
Johannes Klug <jk@jklug.com>
2010-10-22 12:42:34 +0200

    SLIP now uses ints internally; tests enhanced

--- hummingbird/protocols/src/main/java/com/logica/hummingbird/protocols/slip/Slip.java
+++ hummingbird/protocols/src/main/java/com/logica/hummingbird/protocols/slip/Slip.java
@@ -18,13 +18,13 @@ import org.apache.commons.lang.ArrayUtils;
  */
 public class Slip extends Observable {
 	
-	private byte end, esc, escEnd, escEsc;
+	private int end, esc, escEnd, escEsc;
 	
 	/**
 	 *  Constructor for a Slip object: pass the agreed-upon characters for 
 	 *  end marker, escape marker, escaped end marker and escaped escape marker.  
 	 */
-	public Slip(byte end, byte esc, byte escEnd, byte escEsc) {
+	public Slip(int end, int esc, int escEnd, int escEsc) {
 		this.end = end;
 		this.esc = esc;
 		this.escEnd = escEnd;
@@ -32,32 +32,44 @@ public class Slip extends Observable {
 	}
 	
 	public byte[] convertToSlip(byte[] input) {
-		byte[] output = ArrayUtils.EMPTY_BYTE_ARRAY;
+		
+		/*
+		 * For this method, we the byte values of our marker and escape character
+		 * 
+		 */
+		byte byteEnd = (byte) (end & 0xFF);
+		byte byteEsc = (byte) (esc & 0xFF);
+//		byte byteEscEnd = (byte) (escEnd & 0xFF);
+//		byte byteEscEsc = (byte) (escEsc & 0xFF);
+		
+		int[] output = ArrayUtils.EMPTY_INT_ARRAY;
 		
 		/*
 		 * Add each byte from the input array to the
 		 */
 		for (byte b : input) {
-			if (b == end) {
+			if (b == byteEnd) {
 				output = ArrayUtils.add(output, escEnd);
-			} else if (b == esc) {
+			} else if (b == byteEsc) {
 				output = ArrayUtils.add(output, escEsc);
 			}
 			output = ArrayUtils.add(output, b);
 		}
 		
 		output = ArrayUtils.add(output, end);
-		return output;
+		
+	
+		
+		return convertToByteArray(output);
 	}
 	
 	public void readFromStream(InputStream is) {
 		
-		byte[] receivedData = ArrayUtils.EMPTY_BYTE_ARRAY;
+		int[] receivedData = ArrayUtils.EMPTY_INT_ARRAY;
 		int b;
 		
 		try {
 			while ((b = is.read()) != -1 ) {
-				
 				/* 
 				 * We don't want the escape bytes.
 				 * We only consider them escape bytes when they are followed
@@ -79,16 +91,18 @@ public class Slip extends Observable {
 						receivedData = ArrayUtils.add(receivedData, esc);
 					} else {
 						if (b == end) {
-							notifyObservers(receivedData);
-							receivedData = ArrayUtils.EMPTY_BYTE_ARRAY;
+							this.setChanged();
+							notifyObservers(convertToByteArray(receivedData));
+							receivedData = ArrayUtils.EMPTY_INT_ARRAY;
 						}
 						// not an escaped esc, adding 'escEsc' and the current byte
 						receivedData = ArrayUtils.add(receivedData, escEsc);
 						receivedData = ArrayUtils.add(receivedData, (byte) b);
 					}
 				} else if (b == end) {
-					notifyObservers(receivedData);
-					receivedData = ArrayUtils.EMPTY_BYTE_ARRAY;
+					this.setChanged();
+					notifyObservers(convertToByteArray(receivedData));
+					receivedData = ArrayUtils.EMPTY_INT_ARRAY;
 				} else {
 					receivedData = ArrayUtils.add(receivedData, (byte) b);
 				}
@@ -98,6 +112,16 @@ public class Slip extends Observable {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	private byte[] convertToByteArray(int[] input) {
+		byte[] outputBytes = ArrayUtils.EMPTY_BYTE_ARRAY;
+		
+		for (int i : input) {
+			outputBytes = ArrayUtils.add(outputBytes, (byte) (i & 0xFF) );
+		}
+		
+		return outputBytes;
 	}
 
 }
--- hummingbird/protocols/src/test/java/com/logica/hummingbird/protocols/slip/SlipTest.java
+++ hummingbird/protocols/src/test/java/com/logica/hummingbird/protocols/slip/SlipTest.java
@@ -2,18 +2,32 @@ package com.logica.hummingbird.protocols.slip;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.Observable;
+import java.util.Observer;
+
 import org.apache.commons.lang.ArrayUtils;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.logica.hummingbird.protocols.hardware.SerialPortDriver;
 
-public class SlipTest {
+public class SlipTest implements Observer {
 	
 	// Setting up the same marker values as those that are hard-coded in the rocket flight computer
-	private static final byte END = -64, ESC = -37, ESCEND = -36, ESCESC = -35;
+	private static final int END = (0xC0 & 0xFF), 
+								ESC = (0xDB & 0xFF), 
+								ESCEND = (0xDC & 0xFF), 
+								ESCESC = (0xDD & 0xFF);
 	
 	private Slip slip;
+	
+	private byte[] receivedBytes;
 
 	@Before
 	public void setUp() throws Exception {
@@ -23,33 +37,55 @@ public class SlipTest {
 
 	@Test
 	public void testConvertToSlip() {
-		assertEquals((byte)0xC0, END);
-		assertEquals((byte)0xDB, ESC);
-		assertEquals((byte)0xDC, ESCEND);
-		assertEquals((byte)0xDD, ESCESC);
 		
 		byte[] outputEmpty = slip.convertToSlip(ArrayUtils.EMPTY_BYTE_ARRAY);
-		assertEquals(END, outputEmpty[0]);
+		assertEquals((byte) (END & 0xFF), outputEmpty[0]);
 		
 		byte[] oneByteArray = new byte[1];
 		oneByteArray[0] = -127;
 		byte[] outputOneByte = slip.convertToSlip(oneByteArray);
 		
 		assertEquals(oneByteArray[0], outputOneByte[0]);
-		assertEquals(END, outputOneByte[1]);
+		assertEquals(END, (int) (outputOneByte[1] & 0xFF));
 		
-		oneByteArray[0] = END;
+		oneByteArray[0] = (byte) (END & 0xFF);
 		outputOneByte = slip.convertToSlip(oneByteArray);
-		assertEquals(ESCEND, outputOneByte[0]);
-		assertEquals(END, outputOneByte[1]);
-		assertEquals(END, outputOneByte[2]);
+		assertEquals(ESCEND, (int) (outputOneByte[0] & 0xFF)) ;
+		assertEquals(END, (int) (outputOneByte[1] & 0xFF));
+		assertEquals(END, (int) (outputOneByte[2] & 0xFF));
 		
-		oneByteArray[0] = ESC;
+		oneByteArray[0] = (byte) (ESC & 0xFF);
 		outputOneByte = slip.convertToSlip(oneByteArray);
-		assertEquals(ESCESC, outputOneByte[0]);
-		assertEquals(ESC, outputOneByte[1]);
-		assertEquals(END, outputOneByte[2]);
+		assertEquals(ESCESC, (int) (outputOneByte[0] & 0xFF));
+		assertEquals(ESC, (int) (outputOneByte[1] & 0xFF));
+		assertEquals(END, (int) (outputOneByte[2] & 0xFF));
 		
+	}
+	
+	@Test
+	public void testReception() throws IOException, InterruptedException {
+		PipedOutputStream os = new PipedOutputStream();
+		
+		final InputStream is = new PipedInputStream(os);
+		os.write(END & 0xFF);
+		slip.addObserver(this);
+		new Thread(
+				new Runnable(){
+					public void run() {
+						slip.readFromStream(is);
+					}
+				}
+		).start();
+		Thread.sleep(100);
+		assertEquals(0, receivedBytes.length);
+	
+		
+
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		receivedBytes = (byte[]) arg;
 	}
 
 }
