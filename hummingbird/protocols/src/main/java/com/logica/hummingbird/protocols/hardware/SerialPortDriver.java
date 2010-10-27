e06d07e1b98be71610f02f79c94d7267de14835f
Joost Oranje <joost@Thunderbird-EGSE.(none)>
2010-10-27 14:24:56 +0200

    Serial Port Driver: moved to proper package, added Linux rxrx library (32-bit)

--- hummingbird/protocols/src/main/java/com/logica/hummingbird/protocols/hardware/SerialPortDriver.java
+++ hummingbird/protocols/src/main/java/com/logica/hummingbird/protocols/hardware/SerialPortDriver.java
@@ -1,5 +1,6 @@
 package com.logica.hummingbird.protocols.hardware;
 
+import java.io.File;
 import java.util.Enumeration;
 
 import gnu.io.CommPortIdentifier;
@@ -7,9 +8,14 @@ import gnu.io.CommPortIdentifier;
 public class SerialPortDriver {
 	
 	public SerialPortDriver() {
-		Enumeration pList = CommPortIdentifier.getPortIdentifiers();
+		
+//		String s = "/src/main/resources/";
+//		System.setProperty("java.library.path", System.getProperty("java.library.path") + File.pathSeparator + s);
+//
+//		System.out.println(System.getProperty("java.library.path"));
+		
+		Enumeration<?> pList = CommPortIdentifier.getPortIdentifiers();
 
-	    // Process the list.
 	    while (pList.hasMoreElements()) {
 	      CommPortIdentifier cpi = (CommPortIdentifier) pList.nextElement();
 	      System.out.print("Port " + cpi.getName() + " ");
---
+++ hummingbird/protocols/src/test/java/com/logica/hummingbird/protocols/hardware/SerialPortDriverTest.java
@@ -0,0 +1,22 @@
+package com.logica.hummingbird.protocols.hardware;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SerialPortDriverTest {
+	
+	private SerialPortDriver driver;
+
+	@Before
+	public void setUp() throws Exception {
+		driver = new SerialPortDriver();
+	}
+
+	@Test
+	public void testSerialPortDriver() {
+		fail("Not yet implemented");
+	}
+
+}
