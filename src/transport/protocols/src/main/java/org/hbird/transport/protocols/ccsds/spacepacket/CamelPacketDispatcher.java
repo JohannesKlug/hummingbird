ecb33d8b896b789e605b8747bd6cf8c85224c233
Johannes Klug <jk@jklug.com>
2011-01-21 19:19:35 +0100

    Implemented part of a Camel-aware PacketDispatcher

--- src/transport/protocols/src/main/java/org/hbird/transport/protocols/ccsds/spacepacket/CamelPacketDispatcher.java
+++ src/transport/protocols/src/main/java/org/hbird/transport/protocols/ccsds/spacepacket/CamelPacketDispatcher.java
@@ -3,9 +3,10 @@ package org.hbird.transport.protocols.ccsds.spacepacket;
 import java.util.Observable;
 import java.util.Observer;
 
-import org.apache.camel.Exchange;
+import org.apache.camel.Endpoint;
 import org.apache.camel.Message;
 import org.apache.camel.impl.DefaultMessage;
+import org.hbird.transport.protocols.ccsds.transferframe.FramePayload;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,7 +15,9 @@ public class CamelPacketDispatcher implements Observer {
 	private final static Logger LOG = LoggerFactory.getLogger(CamelPacketDispatcher.class);
 	
 	private CcsdsPacketDispatcher packetDispatcher = new CcsdsPacketDispatcher();
-	private Object returnedObject;
+	
+	// FIXME: inject a Camel thingy here.
+	Endpoint endpoint;
 	
 	public CamelPacketDispatcher() {
 		packetDispatcher.addObserver(this);
@@ -22,28 +25,33 @@ public class CamelPacketDispatcher implements Observer {
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
-		returnedObject = arg1;
-	}
-	
-	public Message process(Exchange exchange) {
-		
-		packetDispatcher.process((byte[]) exchange.getIn().getBody(), true);
-		Message message = new DefaultMessage();
-		
-		if (returnedObject instanceof PacketPayload) {
-			PacketPayload payload = (PacketPayload) returnedObject;
+		if (arg1 instanceof PacketPayload) {
+			PacketPayload payload = (PacketPayload) arg1;
 			LOG.info("Processed packet with ApId: " + payload.apid);
+			DefaultMessage message = new DefaultMessage();
 			message.setHeader("ApId", payload.apid);
 			message.setBody(payload.payload);
+			// FIXME send this message somewhere. 
 		} else {
 			LOG.error("Returned object was not a PacketPayload");
 		}
+	}
+	
+	public void process(Message message) {
 		
-		return message;
-		
-		
-		
-		
+		if (message.getBody() instanceof FramePayload) {
+			
+			FramePayload payload = (FramePayload) message.getBody();
+			
+			LOG.info("Processing frame from spacecraft " + payload.spacecraftId 
+					+ " on virtual channel " + payload.vcId 
+					+ ". This is the next frame: " + payload.isNextFrame);
+
+			packetDispatcher.process(payload.payload, payload.isNextFrame);
+			
+		} else {
+			LOG.error("I was given an object that is not a FramePayload");
+		}
 	}
 
 }
