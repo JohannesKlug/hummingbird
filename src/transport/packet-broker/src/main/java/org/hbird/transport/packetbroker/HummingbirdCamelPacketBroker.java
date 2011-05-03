5561a4308c720d26dac35dbe57e5fd8ca3d548c0
TobiasTurba <mail@tturba.de>
2011-05-03 11:55:56 +0200

    Deleted 'copyright by Logica' statement in packetbroker

--- src/transport/packet-broker/src/main/java/org/hbird/transport/packetbroker/HummingbirdCamelPacketBroker.java
+++ src/transport/packet-broker/src/main/java/org/hbird/transport/packetbroker/HummingbirdCamelPacketBroker.java
@@ -1,3 +1,8 @@
+/**
+ * Licensed under the to the Apache License, Version 2.0. You may obtain a copy of 
+ * the License at http://www.apache.org/licenses/LICENSE-2.0 or at this project's root.
+ */
+
 package org.hbird.transport.packetbroker;
 
 import java.util.ArrayList;
