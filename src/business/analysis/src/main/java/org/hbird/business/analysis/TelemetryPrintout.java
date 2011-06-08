dbf9290389bffd183e7f1cfef3b6cc09f144f5f1
Johannes Klug <jk@jklug.com>
2011-06-08 12:49:01 +0200

    Added System.out output to TM printout as a fallback

--- src/business/analysis/src/main/java/org/hbird/business/analysis/TelemetryPrintout.java
+++ src/business/analysis/src/main/java/org/hbird/business/analysis/TelemetryPrintout.java
@@ -13,6 +13,7 @@ public class TelemetryPrintout {
 	// http://camel.apache.org/log.html
 	public static void logParameter(@Header("ParameterName") String name, @Body Object value) {
 		LOG.info(name + ": " + value);
+		System.out.println(name + ": " + value);
 	}
 
 }
