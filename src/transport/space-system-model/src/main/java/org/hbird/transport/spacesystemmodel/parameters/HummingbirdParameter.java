8f7725f9b1f0ceaf897e254d84bd3d43b2e4eddf
Mark Doyle <markjohndoyle@googlemail.com>
2011-08-26 00:31:09 +0200

    Added null check to value when HummingbirdParameter is comparing it's value.

--- src/transport/space-system-model/src/main/java/org/hbird/transport/spacesystemmodel/parameters/HummingbirdParameter.java
+++ src/transport/space-system-model/src/main/java/org/hbird/transport/spacesystemmodel/parameters/HummingbirdParameter.java
@@ -69,6 +69,9 @@ public class HummingbirdParameter<T> implements Parameter<T> {
 
 	@Override
 	public boolean isValue(final Object obj) {
+		if (value == null) {
+			return false;
+		}
 		if (value.equals(obj)) {
 			return true;
 		}
