9de7ced90a9e2daab08fce4e1f432c6a2e4671d3
Mark Doyle <markjohndoyle@googlemail.com>
2011-10-08 13:55:47 +0200

    Added new space system model exception.

---
+++ src/core/xtce/src/main/java/org/hbird/transport/xtce/exceptions/UnsupportedXtceConstructException.java
@@ -0,0 +1,10 @@
+package org.hbird.transport.xtce.exceptions;
+
+public class UnsupportedXtceConstructException extends Exception {
+	private static final long serialVersionUID = -7440614147269195221L;
+
+	public UnsupportedXtceConstructException(final String msg) {
+		super(msg);
+	}
+
+}
