5f29fe788728832d25f6290baefca02898ff32cd
Mark Doyle <markjohndoyle@googlemail.com>
2011-10-09 13:57:06 +0200

    New Encoding constructor added.

--- src/core/space-system-model/src/main/java/org/hbird/transport/spacesystemmodel/encoding/Encoding.java
+++ src/core/space-system-model/src/main/java/org/hbird/transport/spacesystemmodel/encoding/Encoding.java
@@ -9,19 +9,25 @@ public class Encoding {
 	private int sizeInBits;
 	private BinaryRepresentation binaryRepresentation;
 
+	public Encoding(final int sizeInBits, final BinaryRepresentation binaryRepresentation) {
+		this.sizeInBits = sizeInBits;
+		this.binaryRepresentation = binaryRepresentation;
+	}
 
 	public int getSizeInBits() {
 		return sizeInBits;
 	}
+
 	public void setSizeInBits(final int sizeInBits) {
 		this.sizeInBits = sizeInBits;
 	}
+
 	public BinaryRepresentation getBinaryRepresentation() {
 		return binaryRepresentation;
 	}
+
 	public void setBinaryRepresentation(final BinaryRepresentation binaryRepresentation) {
 		this.binaryRepresentation = binaryRepresentation;
 	}
-
 
 }
