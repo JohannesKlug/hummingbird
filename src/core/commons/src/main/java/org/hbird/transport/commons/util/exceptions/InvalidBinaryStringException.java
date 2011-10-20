e227d75d4c1033bdc7065dea03edfb906dac6f8d
Johannes Klug <jk@jklug.com>
2011-10-20 13:19:12 +0200

    Ridiculously defensive programming!

--- src/core/commons/src/main/java/org/hbird/transport/commons/util/exceptions/InvalidBinaryStringException.java
+++ src/core/commons/src/main/java/org/hbird/transport/commons/util/exceptions/InvalidBinaryStringException.java
@@ -8,7 +8,7 @@ public class InvalidBinaryStringException extends Exception {
 	private static final long serialVersionUID = -5587651701214652273L;
 	
 	public InvalidBinaryStringException(int length) {
-		super("Binary string of length " + length + " encountered. This is invalid. Length must be a multiple of 8.");
+		super("Binary string of length " + length + " encountered. This is invalid. Length must be a multiple of " + Byte.SIZE + ".");
 	}
 
 }
