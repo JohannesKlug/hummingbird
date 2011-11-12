a13f459785cec39341603044db5306244afe3534
Johannes Klug <jk@jklug.com>
2011-11-12 15:21:52 +0100

    Added naive type converter that returns a multilist of parameters suited for csv output. This should probably be broken apart to support large lists of payload (Currently, all has to fit into memory at once)

---
+++ src/core/space-system-model/src/main/java/org/hbird/transport/spacesystemmodel/typeconverters/ParameterGroupListToMultiList.java
@@ -0,0 +1,42 @@
+package org.hbird.transport.spacesystemmodel.typeconverters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.camel.Converter;
+import org.hbird.transport.spacesystemmodel.parameters.Parameter;
+import org.hbird.transport.spacesystemmodel.tmtcgroups.ParameterGroup;
+
+@Converter
+public class ParameterGroupListToMultiList {
+	
+	@Converter
+	public static List<List<String>> convertPgListToMultimap(List<ParameterGroup> parameterGroups) {
+		
+		List<List<String>> multilist = new ArrayList<List<String>>();
+		
+		if (parameterGroups.isEmpty() == false) {
+			List<String> headerLine = new ArrayList<String>();
+			headerLine.add("Time Stamp");
+			for (String parameterName : parameterGroups.get(0).getIntegerParameters().keySet()) {
+				headerLine.add(parameterName);
+			}
+			
+			multilist.add(headerLine);
+			
+			for (ParameterGroup pg : parameterGroups) {
+				//Collection<Parameter<Integer>> integerParameters;
+				List<String> bodyLine = new ArrayList<String>();
+				bodyLine.add(Long.toString(pg.getTimeStamp()));
+				for (Parameter<Integer> integerParameter : pg.getIntegerParameters().values()) {
+					bodyLine.add(integerParameter.getValue().toString());
+				}
+				multilist.add(bodyLine);
+			}
+			
+		}
+		
+		return multilist;
+	}
+
+}
