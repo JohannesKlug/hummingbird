7c8401eb03b0e928f0ea999574763f135246f9ff
Mark Doyle <markjohndoyle@googlemail.com>
2012-05-29 17:37:25 +0200

    Added support for archiving ParameterGroups.

--- src/application/parameter-archive/src/main/java/org/hbird/application/parameterarchive/interfaces/ParameterArchiver.java
+++ src/application/parameter-archive/src/main/java/org/hbird/application/parameterarchive/interfaces/ParameterArchiver.java
@@ -1,7 +1,11 @@
 package org.hbird.application.parameterarchive.interfaces;
 
 import org.hbird.core.commons.tmtc.Parameter;
+import org.hbird.core.commons.tmtc.ParameterGroup;
 
 public interface ParameterArchiver {
+
 	void archiveParameter(Parameter<?> parameter);
+
+	void archiveParameter(ParameterGroup parameterGroup);
 }
--- src/application/parameter-archive/src/main/java/org/hbird/application/parameterarchive/mongodb/SimpleMongoParameterArchiver.java
+++ src/application/parameter-archive/src/main/java/org/hbird/application/parameterarchive/mongodb/SimpleMongoParameterArchiver.java
@@ -2,6 +2,7 @@ package org.hbird.application.parameterarchive.mongodb;
 
 import org.hbird.application.parameterarchive.interfaces.ParameterArchiver;
 import org.hbird.core.commons.tmtc.Parameter;
+import org.hbird.core.commons.tmtc.ParameterGroup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,15 @@ public class SimpleMongoParameterArchiver implements ParameterArchiver {
 		mongo.save(parameter, collection);
 	}
 
+	@Override
+	public void archiveParameter(final ParameterGroup parameterGroup) {
+		for(Parameter<?> parameter : parameterGroup.getAllParameters().values()) {
+			mongo.save(parameter, collection);
+		}
+	}
+
 	public void setCollection(final String collection) {
 		this.collection = collection;
 	}
+
 }
