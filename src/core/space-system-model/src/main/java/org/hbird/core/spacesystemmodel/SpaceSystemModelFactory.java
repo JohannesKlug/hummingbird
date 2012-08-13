5894ba4eb42bc30d77ae57412854c73498b36416
Mark Doyle <markjohndoyle@googlemail.com>
2012-08-14 00:12:18 +0200

    Added JSR311 RESTful annotations to Space System Model Factory interface.

--- src/core/space-system-model/src/main/java/org/hbird/core/spacesystemmodel/SpaceSystemModelFactory.java
+++ src/core/space-system-model/src/main/java/org/hbird/core/spacesystemmodel/SpaceSystemModelFactory.java
@@ -1,10 +1,16 @@
 package org.hbird.core.spacesystemmodel;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
 import org.hbird.core.spacesystemmodel.exceptions.InvalidParameterTypeException;
 import org.hbird.core.spacesystemmodel.exceptions.InvalidSpaceSystemDefinitionException;
 
+@Path("ssmfactory")
 public interface SpaceSystemModelFactory {
 
+	@GET
+	@Path("model")
 	SpaceSystemModel createSpaceSystemModel() throws InvalidParameterTypeException, InvalidSpaceSystemDefinitionException;
 
 }
