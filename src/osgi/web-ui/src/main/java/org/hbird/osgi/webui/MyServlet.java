d6e9b14d9500cf9006a35f603550f53d2b179167
Johannes Klug <johannes.klug@cgi.com>
2012-08-27 17:36:47 +0200

    Servlet outputs plain text

--- src/osgi/web-ui/src/main/java/org/hbird/osgi/webui/MyServlet.java
+++ src/osgi/web-ui/src/main/java/org/hbird/osgi/webui/MyServlet.java
@@ -1,6 +1,7 @@
-package my;
+package org.hbird.osgi.webui;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,8 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 
 public class MyServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // your logic here
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -1689860299877290542L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	PrintWriter out = response.getWriter();
+        out.println("Hello World");
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
