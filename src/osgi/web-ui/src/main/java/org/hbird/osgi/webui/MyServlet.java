3cd472c7b37e00dedb26bf706b78ea0db17ac067
Johannes Klug <johannes.klug@cgi.com>
2012-08-28 16:28:07 +0200

    Added web.xml, basic web-ui working

--- src/osgi/web-ui/src/main/java/org/hbird/osgi/webui/MyServlet.java
+++ src/osgi/web-ui/src/main/java/org/hbird/osgi/webui/MyServlet.java
@@ -16,12 +16,13 @@ public class MyServlet extends HttpServlet {
 	private static final long serialVersionUID = -1689860299877290542L;
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("servlet GET called");
     	PrintWriter out = response.getWriter();
         out.println("Hello World");
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // your logic here
+    	System.out.println("servlet POST called");
     }
 
 }
