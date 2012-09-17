6b37092b5803753ab0360dd1affbd8eb62fbe5c7
Mark Doyle <markjohndoyle@googlemail.com>
2012-09-17 16:58:59 +0200

    RT chart example set for air pressure parameter in example system.

--- src/application/web-client/src/main/webapp/pages/telemetry/telemetry.js
+++ src/application/web-client/src/main/webapp/pages/telemetry/telemetry.js
@@ -6,19 +6,34 @@ var rootURL = "http://"+ host + ":" + port + url;
 
 var ws = $.gracefulWebSocket("ws://"+ host + ":" + port + url + "tmsock");
 
+var smoothie = new SmoothieChart({
+//	grid: { 
+//		lineWidth: 1, 
+//		millisPerLine: 1000, 
+//		verticalSections: 6,
+//	},
+	labels: { 
+		fillStyle:'rgb(255, 255, 255)' 
+	}
+});
+
+
+var plotLines = new Array();
+
+//-----------------
+
 ws.onopen = function() {
 	console.log("Websocket connection established.");
 };
 
 ws.onmessage = function(event) {
-	console.log("Received message on socket");
-	plotParameter(event.data);   
+	plotParameter($.parseJSON(event.data));   
 };
 
 jQuery(document).ready(function() {
 	getTelemetryList();
+	smoothie.streamTo(document.getElementById("tmRealTimeChart"), 1000 /*delay*/); 
 });
-
 
 function getTelemetryList() {
 	var jqxhr = $.getJSON(rootURL + "telemetrylist");
@@ -39,6 +54,23 @@ function updateTelemetry(param) {
 	);
 }
 
+var line1 = new TimeSeries( { strokeStyle:'rgb(0, 255, 0)', fillStyle:'rgba(0, 255, 0, 0.4)', lineWidth:3 });
 function plotParameter(parameter) {
-	console.log(parameter);
+	if(parameter.qualifiedName == "Thunderbird.tm.Temperature_BMP") {
+	
+//		line = getLine(parameter.qualifiedName);
+
+		setInterval(function() {
+			line1.append(new Date().getTime(), parameter.value);
+		}, 1000);
+
+		smoothie.addTimeSeries(line1);
+	}
 }
+
+function getLine(qualifiedName) {
+	
+}
+
+
+
