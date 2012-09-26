137e58b9f28817a08ff91efc840a90b861430183
Mark Doyle <markjohndoyle@googlemail.com>
2012-09-26 11:36:25 +0200

    Refactored commanding interface names.

--- src/application/halcyon/src/main/java/org/hbird/application/halcyon/CommandingResource.java
+++ src/application/halcyon/src/main/java/org/hbird/application/halcyon/CommandingResource.java
@@ -5,7 +5,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
-import org.hbird.application.commanding.interfaces.processing.Commanding;
+import org.hbird.application.commanding.interfaces.processing.CommandAcceptor;
 import org.hbird.application.halcyon.osgi.OsgiReady;
 import org.hbird.core.commons.tmtc.CommandGroup;
 import org.slf4j.Logger;
@@ -18,7 +18,7 @@ import com.sun.jersey.spi.resource.Singleton;
 public class CommandingResource extends OsgiReady {
 	private final static Logger LOG = LoggerFactory.getLogger(CommandingResource.class);
 
-	private static final String COMMANDING_SERVICE_NAME = "org.hbird.application.commanding.interfaces.processing.Commanding";
+	private static final String COMMANDING_SERVICE_NAME = "org.hbird.application.commanding.interfaces.processing.CommandAcceptor";
 
 	public CommandingResource() {
 		super(COMMANDING_SERVICE_NAME);
@@ -28,7 +28,7 @@ public class CommandingResource extends OsgiReady {
 	@Path("/sendcommand")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void sendCommand(final CommandGroup cmd) {
-		final Commanding cmdService = (Commanding) getServiceTracker().getService();
+		final CommandAcceptor cmdService = (CommandAcceptor) getServiceTracker().getService();
 
 		if(cmdService != null) {
 			cmdService.acceptCommand(cmd);
