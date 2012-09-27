a2e20b48e2589305b0740f3d5e02e9d657264add
Mark Doyle <markjohndoyle@googlemail.com>
2012-09-27 10:34:57 +0200

    Added test with slew unit test to rotor sim.

--- src/test/rotor/src/test/java/org/hbird/test/rotor/RotorSimulatorTest.java
+++ src/test/rotor/src/test/java/org/hbird/test/rotor/RotorSimulatorTest.java
@@ -9,7 +9,6 @@ import org.hbird.core.commons.tmtc.exceptions.UnknownParameterException;
 import org.hbird.core.spacesystemmodel.exceptions.UnknownParameterGroupException;
 import org.hbird.core.spacesystempublisher.interfaces.SpaceSystemPublisher;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -32,10 +31,6 @@ public class RotorSimulatorTest {
 	@Mock
 	private Parameter<Integer> mockElevationParameter;
 
-	private final static int TGT_ZERO = 0;
-	private final static int TGT_90 = 90;
-	private final static int TGT_180 = 180;
-
 	@Before
 	public void setupMockModel() throws UnknownParameterException, UnknownParameterGroupException {
 		when(mockPublisher.getParameterGroup("Stock6.tm.PositionPayload")).thenReturn(mockParameterGroup);
@@ -52,24 +47,27 @@ public class RotorSimulatorTest {
 	@Test
 	public void testTickNoSlew() throws UnknownParameterGroupException, UnknownParameterException {
 		// Set targets to 0, should not cause a slew
-		sim.slewRotor(TGT_ZERO, TGT_ZERO);
+		sim.slewRotor(0, 0);
 
 		sim.tick();
 
-		verify(mockAzimuthParameter).setValue(TGT_ZERO);
-		verify(mockElevationParameter).setValue(TGT_ZERO);
+		verify(mockAzimuthParameter).setValue(0);
+		verify(mockElevationParameter).setValue(0);
 	}
 
 	@Test
-	@Ignore
-	public void testTickWithSlew() throws UnknownParameterGroupException, UnknownParameterException {
+	public void testTickWithSlew() throws UnknownParameterGroupException, UnknownParameterException, InterruptedException {
 		// Set targets to 0, should not cause a slew
-		sim.slewRotor(TGT_180, TGT_90);
+		sim.slewRotor(10, 9);
 
+		// Give the rotor sim time to move to target.
+		Thread.sleep(2000);
+
+		// tick telemetry generation
 		sim.tick();
 
-		verify(mockAzimuthParameter).setValue(TGT_180);
-		verify(mockElevationParameter).setValue(TGT_90);
+		verify(mockAzimuthParameter).setValue(10);
+		verify(mockElevationParameter).setValue(9);
 	}
 
 }
