e4a42297747046f96859ff0dd20098c846760474
markjohndoyle <markjohndoyle@googlemail.com>
2013-01-17 17:33:32 +0100

    Fixed null pointer in test.

--- src/transport/protocols/src/test/java/org/hbird/transport/protocols/kiss/KissFrameDecoderTest.java
+++ src/transport/protocols/src/test/java/org/hbird/transport/protocols/kiss/KissFrameDecoderTest.java
@@ -1,13 +1,15 @@
 package org.hbird.transport.protocols.kiss;
 
 import org.apache.mina.core.buffer.IoBuffer;
+import org.apache.mina.core.service.TransportMetadata;
+import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.CumulativeProtocolDecoder;
 import org.apache.mina.filter.codec.ProtocolDecoderOutput;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -18,19 +20,23 @@ public class KissFrameDecoderTest {
 	@Mock
 	private ProtocolDecoderOutput mockOut;
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+	@Mock
+	private IoSession mockSession;
+
+	@Mock
+	private TransportMetadata mockTransportMetadata;
 
 	@Before
 	public void setUp() throws Exception {
+		Mockito.when(mockSession.getTransportMetadata()).thenReturn(mockTransportMetadata);
+		Mockito.when(mockTransportMetadata.hasFragmentation()).thenReturn(false);
 	}
 
 	@Test
 	public void testKissFrameDecode() throws Exception {
 		byte[] testInput = new byte[] { (byte) 0xC0, 0x08, 0x1F, (byte) 0xC0 };
 
-		decoder.decode(null, IoBuffer.wrap(testInput), mockOut);
+		decoder.decode(mockSession, IoBuffer.wrap(testInput), mockOut);
 	}
 
 }
