aa7978cb633f1861d81395086cb1ec688763c2e0
markjohndoyle <markjohndoyle@googlemail.com>
2013-02-11 11:09:30 +0100

    Implemented and tested decode byte array for Long twos complement codec.

--- src/transport/payload-codec/src/main/java/org/hbird/transport/payloadcodec/codecparameters/number/TwosComplementLongCodecParameter.java
+++ src/transport/payload-codec/src/main/java/org/hbird/transport/payloadcodec/codecparameters/number/TwosComplementLongCodecParameter.java
@@ -1,8 +1,12 @@
 package org.hbird.transport.payloadcodec.codecparameters.number;
 
+import static java.nio.ByteOrder.LITTLE_ENDIAN;
+
 import java.math.BigInteger;
+import java.nio.ByteBuffer;
 import java.util.BitSet;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.hbird.core.commons.util.BitSetUtility;
 import org.hbird.core.spacesystemmodel.encoding.Encoding;
 import org.hbird.core.spacesystemmodel.tmtc.Parameter;
@@ -13,31 +17,62 @@ import org.slf4j.LoggerFactory;
 /**
  * How do we handle unsigned longs in Java? Long is always signed. Perhaps some math package? Mark: Looks like we must
  * use {@link BigInteger}
- *
+ * 
  * @author Mark Doyle
  * @author Johannes Klug
- *
+ * 
  */
 public class TwosComplementLongCodecParameter extends CodecParameter<Long> {
+	private static final byte ZERO_BYTE = (byte) 0x00;
+
 	private static final long serialVersionUID = -6520952692018179861L;
+
 	/** Logger for this class */
 	private static final Logger LOG = LoggerFactory.getLogger(TwosComplementLongCodecParameter.class);
-
 
 	public TwosComplementLongCodecParameter(final Parameter<Long> hostParameter, final Encoding encoding) {
 		super(hostParameter, encoding);
 	}
 
-
 	@Override
 	public void decode(final byte[] inBytes, final int offset) {
-		throw new UnsupportedOperationException();
-	}
+		// We are going to use Bytebuffer to eventually read out a long value so we need a long sized buffer.
 
+		ByteBuffer fullLengthLongBuf = ByteBuffer.allocate(Long.SIZE);
+
+		// First grab the byte array version of the value.
+		ByteBuffer buf = ByteBuffer.wrap(inBytes);
+		// Work out how many bytes we need to hold the value first
+		int requiredBytes = encoding.getSizeInBits() / Byte.SIZE;
+		if (encoding.getSizeInBits() % Byte.SIZE != 0) {
+			requiredBytes++;
+		}
+		// Now create the array and grab the value
+		byte[] longBytes = new byte[requiredBytes];
+		buf.get(longBytes, offset, requiredBytes);
+
+		// If it's a little endian representation then reverse it so we can work in big-endian (easier for ByteBuffer)
+		if (encoding.getByteOrder() == LITTLE_ENDIAN) {
+			ArrayUtils.reverse(longBytes);
+		}
+
+		// work out if we need to pad and how much.
+		int pad = Long.SIZE - encoding.getSizeInBits();
+
+		if (pad > 0) {
+			// pad left to 64 i.e. byte 8. We have reversed any little-endian data so we cna pad left.
+			for (int i = 0; i < pad / Byte.SIZE; i++) {
+				fullLengthLongBuf.put(ZERO_BYTE);
+			}
+		}
+		fullLengthLongBuf.put(longBytes, 0, requiredBytes).flip();
+
+		setValue(fullLengthLongBuf.getLong());
+	}
 
 	// FIXME String implementation for decoding is nasty!
 	@Override
-	public void decode(final BitSet inBitset,  final int offset) {
+	public void decode(final BitSet inBitset, final int offset) {
 		long parameterValue = 0;
 
 		String binaryString = BitSetUtility.bitSetToBinaryString(inBitset, false);
@@ -58,7 +93,6 @@ public class TwosComplementLongCodecParameter extends CodecParameter<Long> {
 
 		this.setValue(parameterValue);
 	}
-
 
 	@Override
 	public byte[] encodeToByteArray(final byte[] targetBytes, final int offset) {
--- src/transport/payload-codec/src/test/java/org/hbird/transport/payloadcodec/codecparameters/number/TwosComplementLongCodecParameterTest.java
+++ src/transport/payload-codec/src/test/java/org/hbird/transport/payloadcodec/codecparameters/number/TwosComplementLongCodecParameterTest.java
@@ -2,6 +2,7 @@ package org.hbird.transport.payloadcodec.codecparameters.number;
 
 import static org.junit.Assert.assertEquals;
 
+import java.nio.ByteOrder;
 import java.util.BitSet;
 
 import org.hbird.core.commons.util.BitSetUtility;
@@ -22,12 +23,17 @@ public class TwosComplementLongCodecParameterTest {
 	private static CodecParameter<Long> codec;
 
 	private static final long TEST_LONG = 2305843397479642193l;
+
 	private static final String TEST_LONG_STR_BE = "0010000000000000000000000101101001100110011101000011010001010001";
+
 	private static BitSet TEST_LONG_BITSET_BE;
 
 	private static final String TEST_MAX_LONG_STR = "0111111111111111111111111111111111111111111111111111111111111111";
+
 	private static BitSet TEST_MAX_LONG_BITSET;
+
 	private static final String TEST_MIN_LONG_STR = "1000000000000000000000000000000000000000000000000000000000000000";
+
 	private static BitSet TEST_MIN_LONG_BITSET;
 
 	@BeforeClass
@@ -76,4 +82,61 @@ public class TwosComplementLongCodecParameterTest {
 		assertEquals(actual, TEST_LONG_BITSET_BE);
 	}
 
+	@SuppressWarnings("static-method")
+	@Test
+	public final void testDecodeByteArrayFullSizeByteBoundExact64Long() {
+		Parameter<Long> hostParameter = new HummingbirdParameter<Long>("", "", "", "");
+		Long testValue = 1252009l;
+
+		byte[] bigEndianBytes = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x13, 0x1A, (byte) 0xA9 };
+		Encoding enc = new Encoding(bigEndianBytes.length * Byte.SIZE, BinaryRepresentation.twosComplement, ByteOrder.BIG_ENDIAN);
+		TwosComplementLongCodecParameter codec = new TwosComplementLongCodecParameter(hostParameter, enc);
+		codec.decode(bigEndianBytes, 0);
+		assertEquals("", testValue, hostParameter.getValue());
+
+		byte[] littleEndianBytes = new byte[] { (byte) 0xA9, 0x1A, 0x13, 0x00, 0x00, 0x00, 0x00, 0x00 };
+		Encoding lendianEnc = new Encoding(littleEndianBytes.length * Byte.SIZE, BinaryRepresentation.twosComplement, ByteOrder.LITTLE_ENDIAN);
+		TwosComplementLongCodecParameter lendianCodec = new TwosComplementLongCodecParameter(hostParameter, lendianEnc);
+		lendianCodec.decode(littleEndianBytes, 0);
+		assertEquals("", testValue, hostParameter.getValue());
+	}
+
+	@SuppressWarnings("static-method")
+	@Test
+	public final void testDecodeByteArrayFullSizeByteBoundLessThan64() {
+		Parameter<Long> hostParameter = new HummingbirdParameter<Long>("", "", "", "");
+		Long testValue = 1252009l;
+
+		byte[] bigEndianBytes = new byte[] { 0x00, 0x00, 0x00, 0x13, 0x1A, (byte) 0xA9 };
+		Encoding enc = new Encoding(bigEndianBytes.length * Byte.SIZE, BinaryRepresentation.twosComplement, ByteOrder.BIG_ENDIAN);
+		TwosComplementLongCodecParameter codec = new TwosComplementLongCodecParameter(hostParameter, enc);
+		codec.decode(bigEndianBytes, 0);
+		assertEquals("", testValue, hostParameter.getValue());
+
+		byte[] littleEndianBytes = new byte[] { (byte) 0xA9, 0x1A, 0x13, 0x00, 0x00, 0x00 };
+		Encoding lendianEnc = new Encoding(littleEndianBytes.length * Byte.SIZE, BinaryRepresentation.twosComplement, ByteOrder.LITTLE_ENDIAN);
+		TwosComplementLongCodecParameter lendianCodec = new TwosComplementLongCodecParameter(hostParameter, lendianEnc);
+		lendianCodec.decode(littleEndianBytes, 0);
+		assertEquals("", testValue, hostParameter.getValue());
+	}
+
+	@SuppressWarnings("static-method")
+	@Test
+	public final void testDecodeByteArrayFullSizeNonBound() {
+		Parameter<Long> hostParameter = new HummingbirdParameter<Long>("", "", "", "");
+		Long testValue = 68719476736l;
+		final int BIT_PACKED_LENGTH = 37;
+
+		byte[] bigEndianBytes = new byte[] { 0x10, 0x00, 0x00, 0x00, 0x00 };
+		Encoding enc = new Encoding(BIT_PACKED_LENGTH, BinaryRepresentation.twosComplement, ByteOrder.BIG_ENDIAN);
+		TwosComplementLongCodecParameter codec = new TwosComplementLongCodecParameter(hostParameter, enc);
+		codec.decode(bigEndianBytes, 0);
+		assertEquals("", testValue, hostParameter.getValue());
+
+		byte[] littleEndianBytes = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x10 };
+		Encoding lendianEnc = new Encoding(BIT_PACKED_LENGTH, BinaryRepresentation.twosComplement, ByteOrder.LITTLE_ENDIAN);
+		TwosComplementLongCodecParameter lendianCodec = new TwosComplementLongCodecParameter(hostParameter, lendianEnc);
+		lendianCodec.decode(littleEndianBytes, 0);
+		assertEquals("", testValue, hostParameter.getValue());
+	}
 }
