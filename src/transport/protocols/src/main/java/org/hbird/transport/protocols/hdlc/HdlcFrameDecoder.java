7461ee356fcba69741911e76b19881e767643ba5
markjohndoyle <markjohndoyle@googlemail.com>
2013-03-14 23:36:15 +0100

    Added CRC CCITT to hdlc decoder.

--- src/transport/protocols/src/main/java/org/hbird/transport/protocols/hdlc/HdlcFrameDecoder.java
+++ src/transport/protocols/src/main/java/org/hbird/transport/protocols/hdlc/HdlcFrameDecoder.java
@@ -4,6 +4,7 @@ import java.nio.ByteBuffer;
 
 import org.hbird.core.commons.util.BytesUtility;
 import org.hbird.core.commons.util.crc.CRC16CCITT;
+import org.hbird.transport.protocols.hdlc.exceptions.CrcFailureException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,12 +27,8 @@ public class HdlcFrameDecoder {
 
 	private final FCS fcs = FCS.CRC_CCITT;
 
-	// Not implemented yet
-	@SuppressWarnings("unused")
 	private byte address = 0;
 
-	// Not implemented yet
-	@SuppressWarnings("unused")
 	private byte control = 0;
 
 	/**
@@ -39,41 +36,45 @@ public class HdlcFrameDecoder {
 	 * 
 	 * @param dataIn
 	 * @return raw decoded HDLC information byte array, that is, the packet in the HDLC frame.
+	 * @throws CrcFailureException
 	 */
-	public final byte[] decode(byte[] dataIn) {
+	public final byte[] decode(byte[] dataIn) throws CrcFailureException {
 		ByteBuffer in = ByteBuffer.wrap(dataIn);
 
 		// TODO Address and Control still to be implemented.
 		address = in.get();
 		control = in.get();
 
+		byte[] hdlcInformation = null;
+
 		if (useChecksum) {
-			boolean corrupt = true;
 			// TODO get checksum here and do calculations
+			int infoSize;
 			switch (fcs) {
 				case CRC_CCITT:
-					short crcCcitt = in.getShort();
+					infoSize = in.remaining() - Short.SIZE / Byte.SIZE;
+					hdlcInformation = new byte[infoSize];
+					in.get(hdlcInformation);
+					long crcCcitt = in.getShort() & 0xFFFF;
 					CRC16CCITT crc = new CRC16CCITT();
-					crc.update(crcCcitt);
-					if (crc.getValue() == crcCcitt) {
-						corrupt = false;
+					byte[] crcData = ByteBuffer.allocate(1 + 1 + infoSize).put(address).put(control).put(hdlcInformation).array();
+					for (byte b : crcData) {
+						crc.update(b);
+					}
+					if (crc.getValue() != crcCcitt) {
+						throw new CrcFailureException("HLDC Frame CRC check failed. CRC = " + crcCcitt + ". CRC calculation value = " + crc.getValue());
 					}
 					break;
 				case CRC_32:
-					int crc32 = in.getInt();
-					break;
+					throw new UnsupportedOperationException("CRC_32 not yet implemented, file an issue");
 				default:
 					break;
 			}
-
-			if (corrupt) {
-				LOG.error("CRC failure");
-				return null;
-			}
 		}
-
-		byte[] hdlcInformation = new byte[in.remaining()];
-		in.get(hdlcInformation);
+		else {
+			hdlcInformation = new byte[in.remaining()];
+			in.get(hdlcInformation);
+		}
 
 		if (LOG.isDebugEnabled()) {
 			LOG.debug("HDLC information = " + BytesUtility.hexDump(hdlcInformation));
